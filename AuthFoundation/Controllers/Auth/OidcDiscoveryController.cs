6a431cbe7274dc5e4e26bc37d1638528f716e5ce
Takeru Kuriyama <takerukuriyama@example.com>
2026-05-26 23:15:56 +0900

    Advertise birthdate OIDC claim

--- AuthFoundation/Controllers/Auth/OidcDiscoveryController.cs
+++ AuthFoundation/Controllers/Auth/OidcDiscoveryController.cs
@@ -32,7 +32,7 @@ namespace AuthFoundation.Controllers.Auth
                 id_token_signing_alg_values_supported = new[] { "RS256" },
                 token_endpoint_auth_methods_supported = new[] { "none", "client_secret_basic" },
                 scopes_supported = new[] { Code.Scope.OPENID, Code.Scope.EMAIL, Code.Scope.PROFILE },
-                claims_supported = new[] { "sub", "name", "email", "iss", "aud", "exp", "iat", "picture" },
+                claims_supported = new[] { "sub", "name", "email", "iss", "aud", "exp", "iat", "picture", "birthdate" },
                 service_documentation = AppConfig.ServiceDocumentationUrl,
                 code_challenge_methods_supported = new[] { "S256" }
             });
