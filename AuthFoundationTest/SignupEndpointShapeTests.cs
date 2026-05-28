82f7776d70dc6e22b8edc38e2ea4f0b0b6274983
Takeru Kuriyama <takerukuriyama@example.com>
2026-05-29 06:02:34 +0900

    ## Taiga #18 新規登録とプロフィール属性を移植
    * 新規登録とプロフィール属性の設計メモを追加
    
    * email/password/name/birth_date登録APIとPortal画面を追加
    
    * ユーザー登録と認証のテストを追加

---
+++ AuthFoundationTest/SignupEndpointShapeTests.cs
@@ -0,0 +1,57 @@
+using AuthFoundation.Common;
+using AuthFoundation.Controllers.Auth;
+using AuthFoundation.Services;
+
+namespace AuthFoundationTest;
+
+[TestClass]
+public sealed class SignupEndpointShapeTests
+{
+    [TestMethod]
+    public void Post_ReturnsCreatedUserProfile()
+    {
+        var users = new InMemoryUserStore();
+        var controller = EndpointTestHelper.WithHttpContext(new SignupController(users));
+        var request = new SignupRequest(
+            "signup-success@example.com",
+            "Passw0rd!",
+            "Signup User",
+            "2001-02-03");
+
+        var ok = EndpointTestHelper.AssertOk(controller.Post(request));
+
+        Assert.AreEqual(200, ok.StatusCode ?? 200);
+        string sub = EndpointTestHelper.ReadProperty<string>(ok.Value, "sub");
+        Assert.IsTrue(sub.StartsWith("user_", StringComparison.Ordinal));
+        Assert.AreEqual("signup-success@example.com", EndpointTestHelper.ReadProperty<string>(ok.Value, "email"));
+        Assert.AreEqual("Signup User", EndpointTestHelper.ReadProperty<string>(ok.Value, "name"));
+        Assert.AreEqual("2001-02-03", EndpointTestHelper.ReadProperty<string>(ok.Value, "birth_date"));
+        Assert.AreEqual(sub, users.Authenticate("signup-success@example.com", "Passw0rd!").Subject);
+    }
+
+    [TestMethod]
+    public void Post_ReturnsBadRequestForInvalidEmail()
+    {
+        var controller = EndpointTestHelper.WithHttpContext(new SignupController(new InMemoryUserStore()));
+        var request = new SignupRequest("invalid", "Passw0rd!", "Signup User", "2001-02-03");
+
+        ErrorOutput error = EndpointTestHelper.AssertError(controller.Post(request), 400);
+
+        Assert.AreEqual("00001", error.ResponseCode);
+        Assert.AreEqual("invalid_request", error.Error);
+        Assert.AreEqual("email is invalid", error.ErrorDescription);
+    }
+
+    [TestMethod]
+    public void Post_ReturnsBadRequestForInvalidBirthDate()
+    {
+        var controller = EndpointTestHelper.WithHttpContext(new SignupController(new InMemoryUserStore()));
+        var request = new SignupRequest("signup-birth@example.com", "Passw0rd!", "Signup User", "2001-13-40");
+
+        ErrorOutput error = EndpointTestHelper.AssertError(controller.Post(request), 400);
+
+        Assert.AreEqual("00001", error.ResponseCode);
+        Assert.AreEqual("invalid_request", error.Error);
+        Assert.AreEqual("some of the input values are incorrect", error.ErrorDescription);
+    }
+}
